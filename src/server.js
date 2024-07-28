6783c055779ddd57fcb428bb2076535589374270
brenofelips <brendo.filipe2050@outlook.com>
2024-07-27 21:48:44 -0300

    finalizando os conceitos sobre stream

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import { randomUUID } from "node:crypto"
+import http from "node:http"
+import { json } from './middlewares/json.js'
+
+const users = []
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
+  await json(req, res)
+
+  if (method === 'GET' && url === '/users') {
+    return res.writeHead(200).end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body;
+    users.push({
+      id: randomUUID(),
+      name,
+      email,
+    })
+  }
+
+  return res.writeHead(404).end()
+})
+
+server.listen(3333, () => console.log("Server is running"))
